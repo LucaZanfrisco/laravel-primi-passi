5e4ed9ff4dd469bfdc9bfefbb64184f3baa02dce
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-04-28 16:18:20 +0200

    Personal homepage and route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'title' => 'Hello World',
+    ];
+    
+    return view('home',$data);
 });
